18b55ba63913f617d12ae2d276656563295873f0
Miran Raznatovic <raznatovicmiran@gmail.com>
2023-10-23 09:29:51 +0200

    added the logo image on desktop

--- src/components/Desktop/DesktopPage/DesktopPage.tsx
+++ src/components/Desktop/DesktopPage/DesktopPage.tsx
@@ -1,10 +1,29 @@
 import { StyledBackgroundGrid } from "../style.ts";
 import DesktopNavbar from "../DesktopNavbar.tsx";
+import logo_dark from "../../../assets/global/logo_dark.png";
 
 const DesktopPage = () => {
   return (
     <StyledBackgroundGrid container>
       <DesktopNavbar />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "80%",
+          width: "100vw",
+        }}
+      >
+        <img
+          src={logo_dark}
+          alt="logo_dark"
+          style={{
+            maxWidth: "555px",
+            opacity: "0.1",
+          }}
+        />
+      </div>
     </StyledBackgroundGrid>
   );
 };
